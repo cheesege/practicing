218dc41a3cd04a472c775b4e2b9c1c70a750abe6
cheesege <cheese5skin@gmail.com>
2024-09-06 17:35:54 +0800

    update on laptop

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
@@ -7,39 +8,13 @@ signed main(){
     //cheese
     int t;
     cin >> t;
-    for(int i=1;i<=t;i++){
-        cout << "TEST " << i << ":'\n";
-        string a;
-        cin >> a;
-        int n;
-        cin >> n;
-        map<char,pair<int,int>> rect;
-        map<char,pair<int,int>> V_path;
-        map<char,pair<int,int>> H_path;
-        vector<int> hw(10005,0);
-        vector<int> vw(10005,0);
-        for(int j=0;j<n;j++){
-            char b;
-            int x,y;
-            cin >> b >> x >> y;
-            rect.insert(make_pair(b, make_pair(x,y)));
-        }
-        cin >> a;
-        int h,v,w;
-        char b;
-        for(int j=0;j<n;j++){
-            cin >> v >> b >> w;
-            V_path.insert({b,{v,w}});
-        }
-        cin >> a;
-        for(int j=0;j<n;j++){
-            cin >> h >> b >> w;
-            H_path.insert({b,{h,w}});
-        }
-        int pos=65;
-        for(auto f:rect){
-            vw[V_path[f.first].second] = vw[V_path[f.first].first]+f.second.first;
-            hw[H_path[f.first].second] = hw[H_path[f.first].first]+f.second.second;
-        }
+    while(t--) {
+        string a,b;
+        float aa,bb;
+        cin >> a >> b;
+        aa = stof(a);
+        bb = stof(b);
+        cout << (int)(aa+bb);
+
     }
 }
